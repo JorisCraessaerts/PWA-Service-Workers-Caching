b4b4ba21f1b32ecc0c9ce3decca9e0c18b5b02ce
Craessaerts Joris <jorcra@AS-40427.linux.ici>
2018-09-07 16:26:32 +0200

    Cache Multiple File with AddAll

--- public/sw.js
+++ public/sw.js
@@ -1,23 +1,39 @@
 
-self.addEventListener('install', function(event) {
+self.addEventListener('install', function (event) {
   console.log('[Service Worker] Installing Service Worker ...', event);
   event.waitUntil(
     caches.open('static')
       .then((cache) => {
         console.log('[Service Worker] Precaching App Shell');
-        cache.add('/');
-        cache.add('/index.html');
-        cache.add('/src/js/app.js');
+        cache.addAll([
+          '/',
+          '/index.html',
+          '/src/js/app.js',
+          '/src/js/feed.js',
+          '/src/js/promise.js',
+          '/src/js/fetch.js',
+          // Browsers that need these polyfills don't support service workers in the first place so these polyfills won't be added. They would only be installed for browsers
+          // That support service workers and these browsers usually don't need these polyfills anyway.
+          // Caching still makes sence for performance because even on browsers that don't need the polyfills, they are requested in the index.html.
+          // Best is to have a build workflow where these files aren't referenced in the index.html on browsers that don't need these files.
+          '/src/js/material.min.js',
+          '/src/css/app.css',
+          '/src/css/feed.css',
+          '/src/images/main-image.jpg',
+          'https://fonts.googleapis.com/css?family=Roboto:400,700',
+          'https://fonts.googleapis.com/icon?family=Material+Icons',
+          'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+        ]);
       })
   );
 });
 
-self.addEventListener('activate', function(event) {
+self.addEventListener('activate', function (event) {
   console.log('[Service Worker] Activating Service Worker ....', event);
   return self.clients.claim();
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function (event) {
   event.respondWith(
     caches.match(event.request)
       .then((response) => {
